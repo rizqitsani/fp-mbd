126ad0183ba18b191b1620d9da2c4edced23a545
rizqitsani <rizqitsani@gmail.com>
2021-06-26 12:08:29 +0700

    fix: product_id does not auto increment

--- database/migrations/2021_06_21_141439_create_products_table.php
+++ database/migrations/2021_06_21_141439_create_products_table.php
@@ -14,7 +14,7 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->smallInteger('product_id')->primary();
+            $table->smallIncrements('product_id')->startingValue(78);
             $table->string('product_name', 40);
             $table->smallInteger('supplier_id')->nullable();
             $table->smallInteger('category_id')->nullable();
