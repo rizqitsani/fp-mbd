c974140940ccda14bdab86834d2bd14a88fd84ae
reyhannaufal <reyhannaufal21@gmail.com>
2021-07-09 21:23:48 +0800

    feat: add modal on delete product

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,7 +14,11 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- script -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer=""></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <style>
         body {
@@ -97,14 +101,32 @@
                                         <td class="flex px-6 py-4 space-x-3 text-sm font-medium whitespace-nowrap">
                                             <a href="{{route('products.edit', ['id' => $product->product_id])}}"
                                                 class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                            <form
-                                                action="{{ route('products.delete', ['id' => $product->product_id]) }}"
-                                                method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit"
-                                                    class="text-red-500 hover:text-red-600 focus:outline-none">Delete</button>
-                                            </form>
+                                                <button type="button" class="text-red-500 hover:text-red-600 " data-toggle="modal" data-target="#exampleModalCenter">
+                                                   Delete
+                                                  </button>
+                                                  <!-- Modal -->
+                                                  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                                      <div class="modal-content">
+                                                        <div class="modal-header">
+                                                          <h5 class="modal-title" id="exampleModalLongTitle">Apakah anda yakin untuk menghapus data ini?</h5>
+                                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                          </button>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                          <button type="button" class="btn btn-light" data-dismiss="modal">Batal</button>
+                                                           <form
+                                                          action="{{ route('products.delete', ['id' => $product->product_id]) }}"
+                                                          method="POST">
+                                                           @csrf
+                                                          @method('DELETE')
+                                                          <button type="submit" class=" btn btn-danger">Hapus Data</button>
+                                                           </form>
+                                                        </div>
+                                                      </div>
+                                                    </div>
+                                                </div>
                                         </td>
                                     </tr>
                                     @endforeach
@@ -130,3 +152,4 @@
 </body>
 
 </html>
+
